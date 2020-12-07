2278cd09c137a7e3940b33d726e9532c70b10af9
miceyjuice <ganjafz@gmail.com>
2020-12-07 01:09:00 +0100

    Checking things up

--- TimeAndTimePeriodApp/Program.cs
+++ TimeAndTimePeriodApp/Program.cs
@@ -9,20 +9,23 @@ namespace TimeAndTimePeriodApp
     {
         static void Main(string[] args)
         {
-            var czas = new Time("13:20:20");
+            var czas = new Time("13:20:00");
             var czas2 = new Time(14,10);
             
-            var okresczasu = new TimePeriod("14:30:30");
+            var okresczasu = new TimePeriod("1:00");
             var okresczasu1 = new TimePeriod(czas, czas2);
             
             Console.WriteLine(czas2.ToString());
             Console.WriteLine(czas.ToString());
             
             
+            
             Console.WriteLine(czas-okresczasu);
             Console.WriteLine(Minus(czas, okresczasu));
             
             Console.WriteLine(czas+okresczasu);
+            Console.WriteLine(okresczasu);
+            
         }
     }
 }
